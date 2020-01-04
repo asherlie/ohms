eb85e150b605f4ed7e2a5cb197eb5b5615303c33
asherlie <lieber.asher@gmail.com>
2020-01-04 03:42:31 -0500

    ohm_cli now copies the first argument provided to the clipboard, created a separate function copy_text()

--- ohm_cli.c
+++ ohm_cli.c
@@ -8,13 +8,23 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 
-int main(){
-      int sock = socket(SOCK_STREAM, AF_UNIX, 0);
-      
+_Bool copy_text(int sock, char* str, int len){
+      /* mktemp */
       struct sockaddr_un sun;
       sun.sun_family = AF_UNIX;
       strcpy(sun.sun_path, "booboo");
 
-      if(connect(sock, (struct sockaddr*)&sun, sizeof(struct sockaddr_un)) == -1)
+      if(connect(sock, (struct sockaddr*)&sun, sizeof(struct sockaddr_un)) == -1){
             perror("connect");
+            return 0;
+      }
+      send(sock, &len, sizeof(int), 0);
+      send(sock, str, sizeof(char)*len, 0);
+
+      return 1;
+}
+
+int main(int a, char** b){
+      int sock = socket(SOCK_STREAM, AF_UNIX, 0);
+      copy_text(sock, b[1], strlen(b[1]));
 }
