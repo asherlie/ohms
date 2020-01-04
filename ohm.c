ea6d472e8a421aecdc023380462b483f5b61f861
asherlie <lieber.asher@gmail.com>
2020-01-04 03:25:33 -0500

    more sensible filenames

--- ohm.c
+++ ohm.c
@@ -1,20 +1,27 @@
-#if 0
-============================
-|  o    h    m      s      |
-| ohm helps manage strings |
-============================
-#endif
 #include <stdio.h>
+#include <string.h>
+
 #include <sys/socket.h>
 #include <sys/un.h>
 
+int handle_conn(int sock){
+      listen(sock, 0);
+      int peer;
+      while(1){
+            int peer = accept(sock, NULL, NULL);
+            if(peer == -1)continue;
+            printf("gotty\n");
+      }
+}
+
 int main(){
       int sock = socket(SOCK_STREAM, AF_UNIX, 0);
-      
       struct sockaddr_un sun;
+
       sun.sun_family = AF_UNIX;
       strcpy(sun.sun_path, "booboo");
 
-      if(connect(sock, (struct sockaddr*)&sun, sizeof(struct sockaddr_un)) == -1)
-            perror("connect");
+      bind(sock, (struct sockaddr*)&sun, sizeof(struct sockaddr_un));
+
+      handle_conn(sock);
 }
