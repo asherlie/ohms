2f65ae7183328af7aa6a0ed2decb3da3148fcd1d
asherlie <lieber.asher@gmail.com>
2020-01-04 03:17:15 -0500

    added daemon file

---
+++ ohm.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+int main(){
+      int sock = socket(SOCK_STREAM, AF_UNIX, 0);
+      
+      struct sockaddr_un sun;
+      sun.sun_family = AF_UNIX;
+      strcpy(sun.sun_path, "booboo");
+
+      if(connect(sock, (struct sockaddr*)&sun, sizeof(struct sockaddr_un)) == -1)
+            perror("connect");
+}
